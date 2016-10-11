00c81d343c82558882079430aeb274796260fbdb
Marc <phyrenight@gmail.com>
2016-10-11 16:05:04 -0400

    Added screen to calulator display screen.Second screen will be used to store the numbers and operations until = is hit

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
-function number(num){
+function number(num) {
   var screen = document.getElementById("printScreen");
   var str = screen.innerHTML;
-  var total = ""
-  console.log(typeof(num));
-  console.log(screen);
-  if(parseInt(str) === 0){
+  var total = "";
+  if (parseInt(str) === 0) {
     total = String(num);
     var removescreen = document.getElementById("screen")
     removescreen.removeChild(screen)
@@ -12,21 +10,81 @@ function number(num){
     var node = document.createTextNode(total)
     screen.appendChild(node);
     screen.setAttribute("id", "printScreen");
-    removescreen.appendChild(screen);   
+    removescreen.appendChild(screen);
+  } else {
+    total = str + String(num);
+    screen.innerHTML = total;
   }
-  else{
+  pastOperation(num)
+}
+
+function pastOperation(num) {
+  var screen = document.getElementById("past");
+  var str = screen.innerHTML;
+  var total = "";
+  if (parseInt(str) === 0) {
+    total = String(num);
+    var removescreen = document.getElementById("screen")
+    removescreen.removeChild(screen)
+    screen = document.createElement("P");
+    var node = document.createTextNode(total)
+    screen.appendChild(node);
+    screen.setAttribute("id", "past");
+    removescreen.appendChild(screen);
+  } else {
     total = str + String(num);
     screen.innerHTML = total;
   }
 }
 
-
-
-/*var num1 = "";
+function operation(operate){
+  var str ="";
+  var screen = document.getElementById("past");
+  //console.log(screen.innerHTML)
+  str += screen.innerHTML;
+  var allinput = "";
+  console.log(str)
+  if(str.length > 0){
+    allinput = str + operate;
+   // console.log(allinput);
+    screen.innerHTML = allinput;
+  }
+  /*if (currentOperator == "+") {
+    num1 = parseFloat(num1) + parseFloat(num2);
+    assignment(num1, operat);
+    currentOperator = operat;
+  } else if (currentOperator == "-") {
+    num1 = parseFloat(num1) - parseFloat(num2);
+    assignment(num1, operat);
+  } else if (currentOperator == "*") {
+    num1 = parseFloat(num1) * parseFloat(num2);
+    assignment(num1, operat);
+  } else if (currentOperator == "/") {
+    num1 = parseFloat(num1) / parseFloat(num2);
+    assignment(num1, operat);
+  }*/
+}
+function solve(){
+  var parentStr = document.getElementById("past");
+  var str =  parentStr.innerHTML;
+  var total = 0;
+  var start = 0;
+  for(var i in str){
+    if(str[i] ==  "+"){
+      console.log(str[i], i);
+      total += parseInt(str.slice(start,i));
+      console.log(total);
+      start = i;
+    }
+  }
+ // console.log(str);
+}
+/*
+var num1 = "";
 var num2 = "";
 var operatorCounter = 0;
 var currentOperator = "";
-
+/*
 function number(num) {
   // 
   if (operatorCounter < 1) {
