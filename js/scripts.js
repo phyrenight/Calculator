b2d488aa3ceb8e8e82ff1d07e6a9c9e0f7a88989
Marc <phyrenight@gmail.com>
2016-10-18 15:29:54 -0400

    cleaned up code( jshint and removed old  commented out code)

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ function number(num) {
     total = str + String(num);
     screen.innerHTML = total;
   }
-  pastOperation(num)
+  pastOperation(num);
 }
 
 function pastOperation(num) {
@@ -34,7 +34,7 @@ function addDecimal(){
   var currentNumber = printScreen.innerHTML + ".";
   var currentInput = pastInputs.innerHTML + ".";
   pastInputs.innerHTML = currentInput;
-  printSrceeninnerHtml = currentNumber;
+  printScreen.innerHtml = currentNumber;
 
 }
 
@@ -45,6 +45,7 @@ function updateScreen(total){
   console.log(currentNum);
   console.log(total);
 }
+
 function operation(operate){
   var str ="";
   var printScr = document.getElementById("printScreen");
@@ -53,10 +54,11 @@ function operation(operate){
   var allinput = "";
   if(str.length > 0){
     allinput = str + operate;
-    printScreen.innerHTML = 0;
+    printScr.innerHTML = 0;
     screen.innerHTML = allinput;
   }
 }
+
 function solve(op){ 
   var printScreen = document.getElementById("printScreen");
   var parentStr = document.getElementById("past");
@@ -65,6 +67,8 @@ function solve(op){
   var numbers = [];
   var start = 0;
   var total = 0;
+  var num1 = 0;
+  var num2 = 0;
   if(str.length > 2){
     for(var i in str){
       if(str[i] == "+"){
@@ -88,11 +92,10 @@ function solve(op){
         start = (parseInt(i) + 1);
       }
     }
-    numbers.push(str.slice(start))
+    numbers.push(str.slice(start));
     num1 = parseFloat(numbers[0]);
     num2 = parseFloat(numbers[1]);
     total = doTheMath(num1, num2, operations[0]);
-    console.log(total)
     var x = 1;
     for(var n  = 2; n < numbers.length; ++n){
       total = doTheMath(parseInt(total), parseInt(numbers[n]), operations[x]);
@@ -103,6 +106,7 @@ function solve(op){
     parentStr.innerHTML = total;
   }
 }
+
 function doTheMath(num1, num2, operation){
   console.log(operation);
   if(operation == "+"){
@@ -115,13 +119,14 @@ function doTheMath(num1, num2, operation){
     return parseFloat(num1) * parseFloat(num2);
   }
   else if(operation == "/"){
+    // add check to make sure theiir is no dividing by 0
     return parseFloat(num1) / parseFloat(num2);
   }
   else{
-    console.log("what")
+    // need a better else
+    console.log("what");
   }
 }
-
 
 function clearScreen(){
   var printScreen = document.getElementById("printScreen");
@@ -129,110 +134,3 @@ function clearScreen(){
   printScreen.innerHTML = 0;
   pastIput.innerHTML = 0;
 }
-  /*var previousOp =" ";
-  var str =  parentStr.innerHTML;
-  var total = 0;
-  var start = 0;
-  console.log("hello");
-  for(var i in str){
-    if(previousOp == " "){
-      total = parseInt(str.slice(start, i));
-      console.log(i);
-      previousOp = str[i];
-      console.log(previousOp);
-      start = i;
-    }
-    else{
-      if(previousOp ==  "+"){
-        console.log(str[i], i);
-        total += parseInt(str.slice(start, i));
-        console.log(total);
-        start = i +1;
-        previousOp = str[i];
-      }
-      else if(previousOp == "*"){
-        console.log(str.slice(start, i));
-        num = parseInt(str.slice(start, i));
-        total = total * num;
-        start = i+1;
-        previousOp = str[i]
-      }
-      else if(previousOp == "-"){
-        console.log(str.slice(start, i));
-        num = parseInt(str.slice(start, i));
-        total = total - num;
-        start = i+1;
-        previousOp = str[i];
-      }
-      else if(previousOp == "/"){
-        console.log(str.slice(start, i));
-        num = parseInt(str.slice(start, i));
-        total = total/num;
-        start = i+1;
-        previousOp = str[i];
-      }
-      else{
-
-      }
-    }
-  }
-  updateScreen(total);
- // console.log(str);
-}*/
-/*
-var num1 = "";
-var num2 = "";
-var operatorCounter = 0;
-var currentOperator = "";
-/*
-function number(num) {
-  // 
-  if (operatorCounter < 1) {
-    num1 = num1.concat(num);
-    printToScreen(num1);
-  } else {
-    num2 = num2.concat(num);
-    printToScreen(num2);
-  }
-}
-
-function printToScreen(num) {
-  screen = document.getElementById("printScreen");
-  screen.innerHTML = num;
-}
-
-function operation(operat) {
-  //args: operat - the next operation to be performed
-  // carries out the mathmatical operations
-  if (operatorCounter === 0) {
-    currentOperator = operat;
-    operatorCounter += 1;
-  } else {
-    if (currentOperator == "+") {
-      num1 = parseFloat(num1) + parseFloat(num2);
-      assignment(num1, operat);
-      currentOperator = operat;
-    } else if (currentOperator == "-") {
-      num1 = parseFloat(num1) - parseFloat(num2);
-      assignment(num1, operat);
-    } else if (currentOperator == "*") {
-      num1 = parseFloat(num1) * parseFloat(num2);
-      assignment(num1, operat);
-    } else if (currentOperator == "/") {
-      num1 = parseFloat(num1) / parseFloat(num2);
-      assignment(num1, operat);
-    }
-  }
-}
-
-function assignment(num, operat) {
-  printToScreen(num);
-  num2 = "";
-  if (operat == "=") {
-    currentOperator = "";
-    operatorCounter = 0;
-  } else {
-    currentOperator = operat;
-    operatorCounter += 1;
-  }
-}*/
