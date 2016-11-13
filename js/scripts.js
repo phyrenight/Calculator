e0edcbcb532bd019c23109717800c90a56640b81
Marc <phyrenight@gmail.com>
2016-11-12 22:59:39 -0500

    fix bug: made changes so that operations can not be added before numbers are added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 function number(num) {
+  /*
+    args: num - a new number to add to the screen
+    function: updates part the screen of the calculator
+  */
   var screen = document.getElementById("printScreen");
   var str = screen.innerHTML;
   if(str.length < 12){
@@ -11,6 +15,10 @@ function number(num) {
 }
 
 function pastOperation(num) {
+  /*
+    args: num is a string of numbers or an operation
+    functinon: uupdates part of the screen
+  */
   var screen = document.getElementById("past");
   var str = screen.innerHTML;
   var total = "";
@@ -18,7 +26,11 @@ function pastOperation(num) {
 }
 
 function combineInputs(str, num){
-  console.log(str);
+  /*
+    args: str - string of numbers
+    num: new number to be added to string
+    returns a string  
+  */
   var total = "";
   if(parseInt(str) === 0) {
     total = String(num);
@@ -40,14 +52,14 @@ function addDecimal(){
   printScreen.innerHtml = currentNumber;
 
 }
-
+/*
 function updateScreen(total){
   var screenAll = document.getElementById("past");
   var currentNum = document.getElementById("printScreen");
   console.log(screenAll);
   console.log(currentNum);
   console.log(total);
-}
+}*/
 
 function operation(operate){
   var str ="";
@@ -55,11 +67,12 @@ function operation(operate){
   var screen = document.getElementById("past");
   str += screen.innerHTML;
   var allinput = "";
-  if(str.length > 0){
+  if(eval(str) != 0){
     allinput = str + operate;
     printScr.innerHTML = 0;
     screen.innerHTML = allinput;
   }
+
 }
 
 function solve(op){ 
