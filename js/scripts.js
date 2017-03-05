ec93c1186df527df1f02a850d4c2b24048e5776f
Marc <phyrenight@gmail.com>
2017-03-05 17:36:52 -0500

    limit the number of DOM accesses

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 (function(){
   "use strict";
 
+  var currentEquation = "";
+
   var zero = document.getElementById("zero");
   var one = document.getElementById("one");
   var two = document.getElementById("two");
@@ -16,7 +18,7 @@
   var division = document.getElementById("division");
   var multiply = document.getElementById("multiply");
   var addition = document.getElementById("addition");
-  var subract = document.getElementById("subtract");
+  var subtract = document.getElementById("subtract");
   var equals = document.getElementById("equals");
   var decimal = document.getElementById("decimal");
 
@@ -46,26 +48,17 @@ function number() {
     args: num - a new number to add to the screen
     function: updates part the screen of the calculator
   */
+  var equation = "";
   var screen = document.getElementById("printScreen");
   var str = screen.innerHTML;
   if(str.length < 12){
-    screen.innerHTML = combineInputs(str, this.innerHTML);
-    pastOperation(this.innerHTML);
+    equation = combineInputs(str, this.innerHTML);
+    screen.innerHTML = equation;
+    currentEquation += equation;
   }
   else{
     alert("Calculator only takes 12 numbers.");
   }
-}
-
-function pastOperation(num) {
-  /*
-    args: num is a string of numbers or an operation
-    functinon: uupdates part of the screen
-  */
-  var screen = document.getElementById("past");
-  var str = screen.innerHTML;
-  var total = "";
-  screen.innerHTML = combineInputs(str, num);
 }
 
 function combineInputs(str, num){
@@ -74,7 +67,7 @@ function combineInputs(str, num){
     num: new number to be added to string
     returns a string  
   */
-  var total = "";
+  var total = "";    // think about removing
   if(parseFloat(str) === 0 && str != "0.") {
     total = String(num);
     return total;
@@ -88,45 +81,34 @@ function combineInputs(str, num){
 function addDecimal(){
   // add check to make sure current number has only on decimal
   var printScreen = document.getElementById("printScreen");
-  var pastInputs = document.getElementById("past");
   var printScreenValue = printScreen.innerHTML.indexOf(".");
   if(printScreenValue < 0){
     var currentNumber = printScreen.innerHTML + ".";
-    var currentInput = pastInputs.innerHTML + ".";
-    pastInputs.innerHTML = currentInput;
+    var currentInput = currentEquation + ".";
+    currentEquation = currentInput;
     printScreen.innerHTML = currentNumber;
   }
 }
-/*
-function updateScreen(total){
-  var screenAll = document.getElementById("past");
-  var currentNum = document.getElementById("printScreen");
-  console.log(screenAll);
-  console.log(currentNum);
-  console.log(total);
-}*/
 
 function operation(){
   var str ="";
   var printScr = document.getElementById("printScreen");
-  var screen = document.getElementById("past");
   str += screen.innerHTML;
   var allinput = "";
-  if(eval(str) !== 0){
-    allinput = str + this.innerHTML;
+  if(eval(currentEquation.slice(-1)) !== 0){
+    allinput = currentEquation + this.innerHTML;
     printScr.innerHTML = 0;
-    screen.innerHTML = allinput;
+    currentEquation = allinput;
   }
-
 }
 
 function solve(){ 
   var printScreen = document.getElementById("printScreen");
-  var parentStr = document.getElementById("past");
-  var str = parentStr.innerHTML;
-  if(isNumeric(str.slice(-1))){
-    parentStr.innerHTML = eval(str);
-    printScreen.innerHTML = eval(str);
+  console.log(currentEquation);
+  if(isNumeric(currentEquation.slice(-1))){
+    currentEquation = eval(currentEquation);
+    console.log(currentEquation)
+    printScreen.innerHTML = eval(currentEquation);
   }
 }
 
@@ -136,8 +118,7 @@ function isNumeric(n){
 
 function clearScreen(){
   var printScreen = document.getElementById("printScreen");
-  var pastIput = document.getElementById("past");
   printScreen.innerHTML = 0;
-  pastIput.innerHTML = 0;
+  currentEquation = 0;
 }
 })();
